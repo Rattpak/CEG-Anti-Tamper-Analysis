6d49b57bcb7e835b0bf1691a83fd1a151c773e38
Rattpak <73729948+Rattpak@users.noreply.github.com>
2025-05-06 00:26:55 -0400

    Update anti-anti-tamper.cpp

--- code/anti-anti-tamper.cpp
+++ code/anti-anti-tamper.cpp
@@ -36,9 +36,9 @@ void CEG::setupCRCHooks() {
 	//meaning once this is hook, we can do anything to fake CRC
 	
 	// No longer needed since the server thread hook is installed
-	//DWORD crcHookAddr = 0x52C8FC;
-	//crc_jmpBackAddr = crcHookAddr + 5;
-	//Hook::initHook((void*)crcHookAddr, crcHook, 5);
+	DWORD crcHookAddr = 0x52C8FC;
+	crc_jmpBackAddr = crcHookAddr + 5;
+	Hook::initHook((void*)crcHookAddr, crcHook, 5);
 
 	/**Setting up stack pointer adjustment**/
 	constexpr std::array<unsigned char, 3> ESP_PATCH = { 0x83, 0xC4, 0x0C };
@@ -51,8 +51,9 @@ void CEG::setupCRCHooks() {
 	/**Setting up ESI instruction**/
 	//This is the ESI patch discussed in the analysis documentation
 	//add esi, 4 ------> mov esi, 1C0 + nop
-	//constexpr std::array<unsigned char, 6> ESI_PATCH = { 0xBE, 0xC0, 0x01, 0x00, 0x00 , 0x90 };
-	//WriteProcessMemory(pHandle, (LPVOID)0x52C8E5, ESI_PATCH.data(), ESI_PATCH.size(), nullptr);
+	// Not used but here for reference
+	constexpr std::array<unsigned char, 6> ESI_PATCH = { 0xBE, 0xC0, 0x01, 0x00, 0x00 , 0x90 };
+	WriteProcessMemory(pHandle, (LPVOID)0x52C8E5, ESI_PATCH.data(), ESI_PATCH.size(), nullptr);
 
 	/**Disabling SHA1 function checks**/
 	//disable SHA1 checks
